a2ae544a7092592ea2f30eb6ad1a900badfd7e37
igorbarrocal <igorbarrocal@hotmail.com>
2025-09-05 18:42:30 -0300

    fix: corrige estrutura dos DTOs de usuário

--- MotoFacil-API/Application/Dtos/UsuariosDTO.cs
+++ MotoFacil-API/Application/Dtos/UsuariosDTO.cs
@@ -1,9 +1,25 @@
 ﻿namespace MotoFacil_API.Application.Dtos
 {
+    
     public class UsuarioDto
     {
-        public int Id { get; set; }    // Retorna o Id gerado
+        public int Id { get; set; }
         public string Nome { get; set; }
         public string Email { get; set; }
+        public List<LinkDto>? Links { get; set; } // HATEOAS
+    }
+
+    
+    public class LinkDto
+    {
+        public LinkDto(string href, string rel, string method)
+        {
+            Href = href;
+            Rel = rel;
+            Method = method;
+        }
+        public string Href { get; set; }
+        public string Rel { get; set; }
+        public string Method { get; set; }
     }
 }
